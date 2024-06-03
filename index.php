b5086b659650b8f6154aa9d7d4e0313528b03b81
kanakahuja <114636598+kanakahuja@users.noreply.github.com>
2024-06-03 19:17:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+    $server = "localhost:3307"; // Specify the port here
+    $username = "root";
+    $password = ""; // No password
+    $database = "kanak"; // Database name
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password,  $database);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    //echo "success connecting to the db";
+    // Collect post variables
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $desc = $_POST['desc'];
+    $sql = "INSERT INTO `kanak` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+
+    // echo $sql;
+
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+        
+    }
+
+    $con->close();
+}   
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="IIT Kharagpur">
+    <div class="container">
+        <h1>Welcome to IIT Kharagpur US Trip form</h1>
+        <p>Enter your details and submit this form to confirm your participation in the trip </p>
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
+        }
+    ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your Age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <button class="btn" type="submit">Submit</button>
+        </form>
+    </div>
+    <script src="index.js"></script>
+  
+</body>
+</html>
